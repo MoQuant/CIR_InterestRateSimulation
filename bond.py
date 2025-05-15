a86fe62cedf069837a96acffde96d8010a16e221
MoQuant <112667516+MoQuant@users.noreply.github.com>
2025-05-15 15:31:13 -0400

    Update bond.py

--- bond.py
+++ bond.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 import ctypes
 
+# Load the C function and define datatypes for the CIR interest rate model
 cd = ctypes.c_double
 ci = ctypes.c_int
 
@@ -14,6 +15,7 @@ gs = ctypes.CDLL("./bond.so")
 gs.InterestRate.argtypes = (cd, cd, cd, cd, cd, ci, ci)
 gs.InterestRate.restype = cd
 
+# Finds mean reverting parameters for the model based on historical treasury yield data
 def Calibrate(x, dt):
     mu = np.mean(x)
     s2 = np.std(x)**2
@@ -24,10 +26,13 @@ def Calibrate(x, dt):
 
     return alpha, mu, sigma
 
+# Load the treasury yields
 rates = pd.read_csv('rates.csv')[::-1]
 
+# Pull one year treasuries
 R = (rates['1 Yr'].values/100.0).tolist()
 
+# Set some initial parameters of the bonds valuation
 par_value = 1000
 coupon_rate = 0.036
 periods = 2
@@ -36,6 +41,7 @@ maturity = 5
 T = maturity*periods
 CF = par_value*coupon_rate/periods
 
+# Extract the parameters
 N = 400
 Paths = 20
 
@@ -43,16 +49,21 @@ dt = T / N
 
 alpha, mu, sigma = Calibrate(R, dt)
 
+# Fetch latest interest rate to start simulation
 R0 = R[-1]
 
 bond_price = 0
 
+# Run the simulation with the C function and generate the discount rate to find the price of the bond
+# Periods = 2 because this is an annual interest rate with semi-annual payments
 for t in range(T):
     R0 = gs.InterestRate(alpha, mu, sigma, R0, dt, N, Paths)
     discount_rate = R0/periods
     bond_price += CF/pow(1 + discount_rate, t + 1)
 
+# Add the last discount to get the complete bond price
 bond_price += par_value/pow(1 + discount_rate, T)
 
+# Print simulated bond price
 print('Bond Price: ', bond_price)
 
